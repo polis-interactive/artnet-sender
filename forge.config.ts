11ba8f2900893ae2a725fca649ff68a34478410f
Bruce Wilcoxon <brucewilcoxon@twigi-bi.io>
2024-10-22 20:49:35 -0500

    Enable build

--- forge.config.ts
+++ forge.config.ts
@@ -1,21 +1,42 @@
 import type { ForgeConfig } from '@electron-forge/shared-types';
 import { MakerSquirrel } from '@electron-forge/maker-squirrel';
+import { MakerZIP } from '@electron-forge/maker-zip';
+import { MakerDeb } from '@electron-forge/maker-deb';
+import { MakerRpm } from '@electron-forge/maker-rpm';
 import { MakerDMG, MakerDMGConfig } from '@electron-forge/maker-dmg';
+import { PublisherGithub, PublisherGitHubConfig } from '@electron-forge/publisher-github'
 import { VitePlugin } from '@electron-forge/plugin-vite';
 import { FusesPlugin } from '@electron-forge/plugin-fuses';
 import { FuseV1Options, FuseVersion } from '@electron/fuses';
 
 const dmgConfig: MakerDMGConfig = {
   appPath: 'dummy',
-  background: './assets/polis_icon.png'
+  icon: './assets/polis_icon.png'
 };
+
+const githubConfig: PublisherGitHubConfig = {
+  repository: {
+    name: "artnet-sender",
+    owner: "polis-interactive"
+  },
+  prerelease: true
+}
 
 const config: ForgeConfig = {
   packagerConfig: {
     asar: true,
+    icon: './assets/polis_icon.png',
+    name: 'Artnet Sender',
+    osxSign: {},
+    osxNotarize: {
+      appleId: process.env['APPLE_ID'] ?? '',
+      appleIdPassword: process.env['APPLE_ID_PASSWORD'] ?? '',
+      teamId: process.env['APPLE_TEAM_ID'] ?? '',
+    }
   },
   rebuildConfig: {},
-  makers: [new MakerSquirrel({}), new MakerDMG(dmgConfig)],
+  makers: [new MakerSquirrel({}), new MakerZIP({}, ['darwin']), new MakerDMG(dmgConfig), new MakerRpm({}), new MakerDeb({})],
+  publishers: [new PublisherGithub(githubConfig)],
   plugins: [
     new VitePlugin({
       // `build` can specify multiple entry builds, which can be Main process, Preload scripts, Worker process, etc.
